d327ca1d8c411bb8887138db740686fa4350d9cf
cesarin360 <josue360cjpimentel@gmail.com>
2021-10-05 22:13:00 -0600

    BdI

---
+++ Bdl_Grupo2_ProyectoFinal_A/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Bdl_Grupo2_ProyectoFinal_A.MvcApplication" Language="C#" %>
---
+++ Bdl_Grupo2_ProyectoFinal_A/Models/Usr/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Bdl_Grupo2_ProyectoFinal_A.Models.Usuarios
+{
+    public class User
+    {
+        public String Nombre;
+        public String Codigo;
+        public String Correo;
+        public String Password;
+
+     
+        public String getnombre()
+        {
+            return Nombre;
+        }
+
+        public void setNombre(String nombre)
+        {
+            this.Nombre = nombre;
+        }
+
+        public String getcorreo()
+        {
+            return Correo;
+        }
+
+        public void setcorreo(String correo)
+        {
+            this.Nombre = correo;
+        }
+
+        public String getcodigo()
+        {
+            return Codigo;
+        }
+
+        public void setcodigo(String codigo)
+        {
+            this.Codigo = codigo;
+        }
+
+        public String getPassword()
+        {
+            return Password;
+        }
+
+        public void setPassword(String password)
+        {
+            this.Password = password;
+        }
+
+    }
+}
